ce6994a915a0344d7104d5f751392ed199235ca8
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-23 05:53:41 +0600

    in BaseRepository add methode doRequest

--- app/src/main/java/com/example/newsapp/base/BaseRepository.kt
+++ app/src/main/java/com/example/newsapp/base/BaseRepository.kt
@@ -1,3 +1,20 @@
 package com.example.newsapp.base
 
-abstract class BaseRepository
+import com.example.newsapp.common.Resource
+import kotlinx.coroutines.flow.flow
+
+abstract class BaseRepository {
+
+    protected fun <T> doRequest(request: suspend () -> T) = flow {
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(data = request()))
+        } catch (e: Exception) {
+            emit(
+                Resource.Error(
+                    data = null, message = e.localizedMessage ?: "Error Occurred"
+                )
+            )
+        }
+    }
+}
