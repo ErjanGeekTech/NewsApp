1cf09ddad312c3525851e90e64bfa99746376d60
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-23 05:53:00 +0600

    replace pagingDataAdapter on ListAdapter

--- app/src/main/java/com/example/newsapp/presentation/ui/adapters/ArticleAdapter.kt
+++ app/src/main/java/com/example/newsapp/presentation/ui/adapters/ArticleAdapter.kt
@@ -1,11 +1,10 @@
-package com.example.newsapp.ui.adapters
+package com.example.newsapp.presentation.ui.adapters
 
 import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.GlideException
@@ -14,12 +13,12 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.example.newsapp.base.BaseDiffUtilItemCallback
 import com.example.newsapp.databinding.ItemEverythingNewsBinding
+import com.example.newsapp.domain.models.EverythingModel
 import com.example.newsapp.extensions.dateFormatTime
-import com.example.newsapp.models.EverythingModel
 
 
-class EverythingAdapter :
-    PagingDataAdapter<EverythingModel, EverythingAdapter.EverythingViewHolder>(
+class ArticleAdapter :
+    ListAdapter<EverythingModel, ArticleAdapter.EverythingViewHolder>(
         BaseDiffUtilItemCallback<EverythingModel>()
     ) {
 
@@ -69,8 +68,7 @@ class EverythingAdapter :
 
             title.text = it.title
             desc.text = it.description
-            source.text = it.source.name
-            Log.e("anime", it.publishedAt)
+            source.text = it.source?.name
             time.text = dateFormatTime(it.publishedAt)
             publishedAt.text = dateFormatTime(it.publishedAt)
             author.text = it.author
