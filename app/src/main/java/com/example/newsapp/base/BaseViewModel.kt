528be5179a51616cd1626bd6f0b49be4cbc35517
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-23 05:54:10 +0600

    add methode subscribeTo

--- app/src/main/java/com/example/newsapp/base/BaseViewModel.kt
+++ app/src/main/java/com/example/newsapp/base/BaseViewModel.kt
@@ -1,5 +1,38 @@
 package com.example.newsapp.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.common.Resource
+import com.example.newsapp.presentation.state.UIState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-open class BaseViewModel : ViewModel()
+abstract class BaseViewModel : ViewModel() {
+
+    protected fun <T> subscribeTo(
+        state: MutableLiveData<UIState<T>>,
+        request: () -> Flow<Resource<T>>
+    ) {
+        viewModelScope.launch {
+            request().collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        state.value = UIState.Loading()
+                    }
+                    is Resource.Error -> {
+                        it.message?.let { error ->
+                            state.value = UIState.Error(error)
+                        }
+                    }
+                    is Resource.Success -> {
+                        it.data?.let { data ->
+                            state.value = UIState.Success(data)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
