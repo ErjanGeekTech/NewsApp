8c035972ff43083c53c502c9b35cfc51c0b97a6a
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-23 06:05:35 +0600

    resource

--- app/src/main/java/com/example/newsapp/common/Resource.kt
+++ app/src/main/java/com/example/newsapp/common/Resource.kt
@@ -1,10 +1,10 @@
-package com.example.newsapp.data.resource
+package com.example.newsapp.common
 
 sealed class Resource<out T>(
     val data: T? = null,
-    val message: String? = null
+    val message: String? = null,
 ) {
     class Loading<T>(data: T? = null) : Resource<T>(data = data)
     class Success<T>(data: T) : Resource<T>(data = data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(message = message, data = data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data = data, message = message)
 }
