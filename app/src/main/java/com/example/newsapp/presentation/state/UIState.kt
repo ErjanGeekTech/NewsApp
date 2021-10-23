a691d12c448cf67f8139715044e8beb9dca45ffa
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-23 06:07:32 +0600

    add state

---
+++ app/src/main/java/com/example/newsapp/presentation/state/UIState.kt
@@ -0,0 +1,7 @@
+package com.example.newsapp.presentation.state
+
+sealed class UIState<T> {
+    class Loading<T> : UIState<T>()
+    class Error<T>(val error: String) : UIState<T>()
+    class Success<T>(val data: T) : UIState<T>()
+}
